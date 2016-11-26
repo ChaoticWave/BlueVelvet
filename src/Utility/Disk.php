6f422997ea6d0c4df26b33f4feb4c3d2df411967
lucifurious <jerryablan@gmail.com>
2016-11-25 20:07:25 -0500

    Composer dependencies updated.

--- src/Utility/Disk.php
+++ src/Utility/Disk.php
@@ -283,4 +283,19 @@ class Disk
     {
         return $separator . ltrim($path, ' ' . $separator);
     }
+
+    /**
+     * Test if an url/path is absolute or not
+     *
+     * @param string $url
+     *
+     * @return bool TRUE if absolute, FALSE if relative
+     */
+    public static function is_absolute($url)
+    {
+        $_regex =
+            "/^(?:ftp|https?|feed):\/\/(?:(?:(?:[\w\.\-\+!$&'\(\)*\+,;=]|%[0-9a-f]{2})+:)*(?:[\w\.\-\+%!$&'\(\)*\+,;=]|%[0-9a-f]{2})+@)?(?:(?:[a-z0-9\-\.]|%[0-9a-f]{2})+|(?:\[(?:[0-9a-f]{0,4}:)*(?:[0-9a-f]{0,4})\]))(?::[0-9]+)?(?:[\/|\?](?:[\w#!:\.\?\+=&@$'~*,;\/\(\)\[\]\-]|%[0-9a-f]{2})*)?$/xi";
+
+        return (bool)preg_match($_regex, $url);
+    }
 }
